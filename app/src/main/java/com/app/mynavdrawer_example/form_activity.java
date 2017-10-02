a404b0d3ce0d5f4a2779c3bc1cb25f388af5cff5
Yacov Rosenberg <yacov94@yahoo.com>
2017-10-02 16:37:15 -0300

    commit

---
+++ app/src/main/java/com/app/mynavdrawer_example/form_activity.java
@@ -0,0 +1,15 @@
+package com.app.mynavdrawer_example;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class form_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_activity);
+
+        setTitle("Formação");
+    }
+}
---
+++ app/src/main/java/com/app/mynavdrawer_example/xp_activity.java
@@ -0,0 +1,15 @@
+package com.app.mynavdrawer_example;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class xp_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_xp_activity);
+
+        setTitle("Experiência Profissional");
+    }
+}
